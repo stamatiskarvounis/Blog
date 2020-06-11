79e248cdcc7756967509abccf2cc1f9b39ce05b8
stamatiskarvounis <stamatiskarvounis@hotmail.com>
2020-06-11 16:39:23 +0100

    Fix widget-teaks2

--- stamtravelblog/settings.py
+++ stamtravelblog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'gallery.apps.GalleryConfig',
+    'widget_tweaks',
     'crispy_forms',
     'contact.apps.ContactConfig',
     'destinations.apps.DestinationsConfig',
